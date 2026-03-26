d75e7934376845e75d140f46fd224e21de88a817
Aadil pathan <pathanab_1@rknec.edu>
2026-03-26 22:16:25 +0530

    Time: 30 ms (28.08%), Space: 23.4 MB (89.41%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
+    typedef pair<int, pair<int, int>> P;
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        if(m==0 || n==0 || grid[0][0]==1) return -1;
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        grid[0][0]=1;
-        int count=0;
-        int diri[]={0,0,-1,+1,+1,-1,+1,-1};
-        int dirj[]={-1,+1,0,0,+1,-1,-1,+1};
-        while(!q.empty()){
-            int size=q.size();
-            count++;
-            while(size--){
-                auto &curr= q.front();
-                int i=curr.first;
-                int j=curr.second;
-                
-                q.pop();
+        int m = grid.size();
+        int n = grid[0].size();
+        if (m == 0 || n == 0 || grid[0][0] == 1) return -1;
+        priority_queue<P, vector<P>, greater<P>> q;
+        vector<vector<int>> res(m, vector<int>(n, INT_MAX));
+        res[0][0] = 0;
+        q.push({0, {0, 0}});
+        int diri[] = {0, 0, -1, +1, +1, -1, +1, -1};
+        int dirj[] = {-1, +1, 0, 0, +1, -1, -1, +1};
+        while (!q.empty()) {
+            auto curr = q.top();
+            int dis = curr.first;
+            int i = curr.second.first;
+            int j = curr.second.second;
+            q.pop();
+            if(dis > res[i][j]) continue;
 
-                if(i==m-1 && j==n-1){
-                    return count;
+            for (int k = 0; k < 8; k++) {
+                int newi = i + diri[k];
+                int newj = j + dirj[k];
+                if (newi >= 0 && newi < m && newj >= 0 && newj < n &&
+                    grid[newi][newj] == 0) {
+                    if (res[newi][newj] > dis + 1) {
+                        res[newi][newj] = dis + 1;
+                        q.push({dis + 1, {newi, newj}});
+                    }
                 }
-
-                for(int k=0;k<8;k++){
-                    int newi=i+diri[k]; 
-                    int newj=j+dirj[k];
-                    if(newi>=0 && newi<m &&newj>=0 && newj<n && grid[newi][newj]==0){
-                        grid[newi][newj]=1;
-                        q.push({newi,newj});
-                    } 
-                }
-                
             }
-            
         }
+        if(res[m-1][n-1]==INT_MAX) return -1;
 
-        return -1;
+        return res[m-1][n-1]+1;
     }
 };
