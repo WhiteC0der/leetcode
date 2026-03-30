151de4a229173c0bd40a1cb1de28b2a619e0f87c
Aadil pathan <pathanab_1@rknec.edu>
2026-03-30 21:55:08 +0530

    Time: 847 ms (15.23%), Space: 180.5 MB (16.2%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,33 +1,73 @@
 class Solution {
 public:
-    typedef pair<int,int> P;
+    vector<int> par;
+    vector<int> rank;
+    
+    int find(int a){
+        if(par[a]==a){
+            return a;
+        }
+        
+        return par[a]=find(par[a]);
+    }
+    
+    void Union(int a,int b){
+        int par_a=find(a);
+        int par_b=find(b);
+        if(par_a != par_b){
+              if(rank[par_a] == rank[par_b]){
+            par[par_b] = par_a;
+            rank[par_a]++;
+        }
+        else if(rank[par_a] > rank[par_b]){
+            par[par_b] = par_a;
+        }
+        else{
+            par[par_a] = par_b;
+        }
+        }
+    }
+  
+    static bool compr(vector<int> &a,vector<int> &b){
+        return a[2] < b[2];
+    }
+    
+    int Kruskals(vector<vector<int>>& edges){
+        int sum=0;
+        for(auto &ed: edges){
+            int u=ed[0];
+            int v=ed[1];
+            int wt=ed[2];
+            if(find(u)!=find(v)){
+                sum+=wt;
+                Union(u,v);
+            }
+        }
+        
+        return sum;
+    }
+
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int,vector<P>> adj;
-        priority_queue<P,vector<P>,greater<P>> q;
         int n = points.size();
+        par.resize(n);
+        rank.resize(n,0);
+        for(int i=0;i<n;i++) par[i]=i;
+        vector<vector<int>> adj;
         vector<bool> vis(n);
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int dis= abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
-                adj[i].push_back({j,dis});
-                adj[j].push_back({i,dis});
+                adj.push_back({i,j,dis});
             }
         }
-        q.push({0,0});
-        int sum=0;
-        while(!q.empty()){
-            auto [dis,node]= q.top();
-            q.pop();
-            if(vis[node]) continue;
-            sum+=dis;
-            vis[node]= true;
-            for(auto &ed: adj[node]){
-                if(!vis[ed.first]){
-                    q.push({ed.second,ed.first});
-                }
-            }
-        }
+        
+        auto compr = [&](vector<int> &a,vector<int> &b){
+            return a[2] < b[2];
+        };
+        
+        sort(adj.begin(),adj.end(),compr);
 
-        return sum;
+
+        return Kruskals(adj);
     }
 };
