a613098fa9d810e9cde92f6b392d6a4c53b9e085
Aadil pathan <pathanab_1@rknec.edu>
2026-04-06 01:04:06 +0530

    Time: 257 ms (45.43%), Space: 157.2 MB (30.36%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     typedef long long ll;
-    ll t[100001][2];
 
-    ll solve(int idx,vector<int>& nums,bool flag){
-        if(idx>=nums.size()){
-            return 0;
-        }
-        if(t[idx][flag] != -1) return t[idx][flag];
-        ll skip = solve(idx+1,nums,flag);
-        int val = nums[idx];
-        if(flag == false){
-            val = -val;
-        }
-        ll take = solve(idx+1,nums,!flag) + val;
-
-        return t[idx][flag] =  max(take,skip);
-    }
     long long maxAlternatingSum(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
+        int n= nums.size();
+        vector<vector<ll>> t(n+1,vector<ll> (2,0));
+        for(int i=1;i<=n;i++){
+            t[i][0] = max(t[i-1][1]+nums[i-1],t[i-1][0]);
 
-        return solve(0,nums,true);
+            t[i][1] = max(t[i-1][0]-nums[i-1],t[i-1][1]);
+        }
+
+        return max(t[n][0],t[n][1]);
     }
 };
