ad0df2b8fd991c657646aeba5076ede66e8456f6
Aadil pathan <pathanab_1@rknec.edu>
2026-06-16 18:26:44 +0530

    Time: 3 ms (76.89%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a' && s[i]<='z'){
+                result+=s[i];
+            }else if(s[i]=='*'){
+                if(!result.empty()) result.pop_back();
+            }else if(s[i]=='#'){
+                result+=result;
+            }else{
+                reverse(result.begin(),result.end());
+            }
+        }
+
+        return result;
+    }
+};
