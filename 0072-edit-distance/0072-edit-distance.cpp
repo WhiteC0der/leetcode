4d5309af59c6b2a2de605554542ad5050fe37aa2
Aadil pathan <pathanab_1@rknec.edu>
2026-06-17 18:20:42 +0530

    Time: 44 ms (12.61%), Space: 10.5 MB (90.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int dp[501][501];
-    int solve(int i,int j,string word1,string word2){
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==0 || j==0){
-            return dp[i][j] = i+j;
-        }
-
-        if(word1[i-1]==word2[j-1]){
-            return dp[i][j] = solve(i-1,j-1,word1,word2);
-        }
-
-        return dp[i][j] = 1 + min({solve(i,j-1,word1,word2),solve(i-1,j,word1,word2),solve(i-1,j-1,word1,word2)});
-    }
     int minDistance(string word1, string word2) {
-        memset(dp,-1,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         int m=word1.length();
         int n=word2.length();
 
-        return solve(m,n,word1,word2); 
+        for(int i=0;i<=m ;i++){
+            dp[i][0]=i;
+        }
+
+        for(int j=0;j<=n ;j++){
+            dp[0][j]=j;
+        }
+
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1 + min({
+                        dp[i][j-1],
+                        dp[i-1][j],
+                        dp[i-1][j-1]
+                    });
+                }
+            }
+        }
+
+        return dp[m][n]; 
     }
 };
