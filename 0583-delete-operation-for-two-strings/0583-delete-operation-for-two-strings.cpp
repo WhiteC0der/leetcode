24b45119b2f6949d36c3b38b4fbbdc99c19bdab9
Aadil pathan <pathanab_1@rknec.edu>
2026-06-23 23:49:45 +0530

    Time: 51 ms (7.33%), Space: 10.8 MB (99.86%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int dp[501][501];
-    int solve(int i,int j,string word1, string word2){
-        if(i==0 || j==0){
-            return i+j;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i-1]==word2[j-1]){
-            return dp[i][j] = solve(i-1,j-1,word1,word2);
-        }
+    // int solve(int i,int j,string word1, string word2){
+    //     if(i==0 || j==0){
+    //         return i+j;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(word1[i-1]==word2[j-1]){
+    //         return dp[i][j] = solve(i-1,j-1,word1,word2);
+    //     }
 
-        return dp[i][j] = 1+ min(solve(i-1,j,word1,word2),solve(i,j-1,word1,word2));
-    }
+    //     return dp[i][j] = 1+ min(solve(i-1,j,word1,word2),solve(i,j-1,word1,word2));
+    // }
     int minDistance(string word1, string word2) {
         memset(dp,-1,sizeof(dp));
         int m,n;
-        m=word1.length();
+        m=word1.length(); 
         n=word2.length();
-        return solve(m,n,word1,word2);
+        
+        for(int i=0;i<=m;i++) dp[i][0] = i;
+        for(int j=0;j<=n;j++) dp[0][j] = j;
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1 + min(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
