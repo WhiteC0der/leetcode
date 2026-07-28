4284d226772aafb443a14538d43fca3f0e15041e
Aadil pathan <pathanab_1@rknec.edu>
2026-07-28 22:36:22 +0530

    Time: 23 ms (82.86%), Space: 77.3 MB (23.95%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,21 +3,23 @@ public:
     string smallestPalindrome(string s) {
         vector<int> freq(26,0);
         string ans="";
+        char mid;
         for(int i=0;i<s.length();i++){
             freq[s[i]-'a']++;
         }
         for(int i=0;i<26;i++){
-            if(freq[i]>=2){
-                ans+=string(freq[i]/2,'a'+i);
-                freq[i]%=2;
-            }
+            if(freq[i]%2!=0){
+                mid='a'+i;
+            } 
+           
+            ans+=string(freq[i]/2,'a'+i);
+                
         }
         string temp = ans;
-        for(int i=0;i<26;i++){
-            if(freq[i]>0) ans+=string(freq[i],'a'+i);
-        }
-
+        if(mid) ans+=mid;
+        
         reverse(temp.begin(),temp.end());
+
         return ans+temp;
     }
 };
