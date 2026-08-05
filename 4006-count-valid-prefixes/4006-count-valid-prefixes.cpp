bb445600202110beadebfc720b8a74c4e0b49356
Aadil pathan <pathanab_1@rknec.edu>
2026-08-05 16:34:20 +0530

    Time: 0 ms (100%), Space: 9.4 MB (85.04%) - LeetHub

---
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countValidPrefixes(string s) {
+        int ans=0;
+        int zero=0;
+        int one=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='0') zero++;
+            else one++;
+            if(abs(zero-one) == 1 || abs(zero-one) == 0) ans++;
+        }
+
+        return ans;
+    }
+};
